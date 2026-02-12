442b1d12dcec89801ee9f235bf5b7256dd24b09d
jahyun03 <jiah03hyun@gmail.com>
2026-02-12 01:05:15 -0500

    2/12

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,5 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [mdx(), sitemap()]
-});
+    site: 'https://giahyun.com'
+  });
